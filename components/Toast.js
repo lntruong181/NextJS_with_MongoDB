c6b8836745395f7d0381c03f1de8410b4239b980
Truongle.dev <72255700+lntruong181@users.noreply.github.com>
2021-03-26 13:43:26 +0700

    Update Toast.js

--- components/Toast.js
+++ components/Toast.js
@@ -1,14 +1,14 @@
 const Toast = ({msg, handleShow, bgColor}) => {
     return(
         <div className={`toast show position-fixed text-light ${bgColor}`}
-        style={{ top: '50px', right: '5px', zIndex: 9, minWidth: '280px' }} >
+        style={{ top: '5px', right: '5px', zIndex: 9, minWidth: '280px' }} >
 
             <div className={`toast-header ${bgColor} text-light`}>
                 <strong className="mr-auto text-light">{msg.title}</strong>
 
                 <button type="button" className="ml-2 mb-1 close text-light" 
                 data-dismiss="toast" style={{ outline: 'none'}} 
-                onClick={handleShow}>x</button>
+                onClick={handleShow}></button>
             </div>
 
             <div className="toast-body">{msg.msg}</div>
