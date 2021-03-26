f05aec3a286d7a6472667741f2d44dd76b21a6df
NguyenHung <ken247199@gmail.com>
2021-03-26 12:32:24 +0700

    add paypal

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
     env: {
         "BASE_URL": "https://next-js-with-mongo-db.vercel.app/",
         "MONGODB_URL": "mongodb+srv://dbUser:dbUser@database-shop.gqq2f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-        "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
-        "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
-        "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
+        "ACCESS_TOKEN_SECRET": "hungrau995922502177098174",
+        "REFRESH_TOKEN_SECRET": "hungrau9962351775rece972199574",
+        "PAYPAL_CLIENT_ID": "AbGivY6IlgzgnbSCfNDfK5LpCPszzaIszVY3OgXWGg14rr4DRN-RGV13c7_adXFbnN35m4r8dG0xqGh_",
         "CLOUD_UPDATE_PRESET": "nodejs_store",
         "CLOUD_NAME": "https-next-js-with-mongo-db-vercel-app",
         "CLOUD_API": "https://api.cloudinary.com/v1_1/https-next-js-with-mongo-db-vercel-app/image/upload"
